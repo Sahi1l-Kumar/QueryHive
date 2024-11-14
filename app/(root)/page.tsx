67065dd7a7054f1dab33e054a8313a53a59d29d0
Sahi1l-Kumar <2230189@kiit.ac.in>
2024-11-14 21:31:40 +0530

    Implement Auth Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/route";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
