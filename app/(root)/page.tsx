e1b5dad7b5a730b118ad6e6711379ac1f7c891cb
Sahi1l-Kumar <2230189@kiit.ac.in>
2024-11-14 22:45:57 +0530

    MobileNav and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -8,15 +6,6 @@ const Home = async () => {
   return (
     <div>
       <h1 className="text-3xl font-black text-white">Query Hive</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </div>
   );
 };
