c4cedfa434a137050ab2aab5169e20737449e672
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-05-08 19:24:49 +0530

    Implement Global Search Feature

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -7,13 +7,15 @@ import UserAvatar from "@/components/UserAvatar";
 
 import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
+import ROUTES from "@/constants/routes";
+import GlobalSearch from "@/components/search/GlobalSearch";
 
 const Navbar = async () => {
   const session = await auth();
 
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href={ROUTES.HOME} className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
           width={23}
@@ -26,7 +28,7 @@ const Navbar = async () => {
         </p>
       </Link>
 
-      <p>Global Search</p>
+      <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
